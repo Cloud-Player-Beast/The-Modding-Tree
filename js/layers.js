68e8dcd41449528666589d3797926b67ce16ca31
Cloud-Player-Beast <123403992+Cloud-Player-Beast@users.noreply.github.com>
2023-02-11 12:31:28 +0800

    实验级更新

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,5 @@
 addLayer("注册", {
-    name: "未知", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "注册", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "注册", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -22,12 +22,12 @@ addLayer("注册", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "$", description: "$: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "注册", description: "注册: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
 		upgrades: {
 		11:{
-		title: "开始",
+		title: "GA",
 		description: "你找到一个免费的放置游戏",
 		cost: new Decimal(0),
 		},
